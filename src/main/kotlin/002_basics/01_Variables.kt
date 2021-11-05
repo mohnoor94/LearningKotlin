99e2d1316a091d80fdd12f0aced93bf811732255
mohnoor94 <moh.noor94@gmail.com>
2021-11-05 21:22:27 +0200

    Variables (val and var)

---
+++ src/main/kotlin/002_basics/01_Variables.kt
@@ -0,0 +1,18 @@
+package `002_basics`
+
+// var: mutable (can be modified)
+// val: read-only (final / cannot be modified)
+
+fun main() {
+    val hello = "hello"
+    println(hello)
+
+    // hello = "hi"  // Error: Val cannot be reassigned
+
+
+    // // the type could be automatically inferred by Kotlin (Explicitly given type is redundant here)
+    var hi: String = "hi"
+    var number: Int = 0
+
+    hi = "hello" // it's a var, you can reassign a new value.
+}
