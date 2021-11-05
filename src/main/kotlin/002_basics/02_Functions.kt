ebe7158ab91e4d28d4c883d7806c16f2ad741046
mohnoor94 <moh.noor94@gmail.com>
2021-11-05 21:33:13 +0200

    Functions

---
+++ src/main/kotlin/002_basics/02_Functions.kt
@@ -0,0 +1,19 @@
+package `002_basics`
+
+fun main() {
+    println("Max is ${max(10, 6)}")
+    displayMax(20, 30)
+}
+
+
+// normal function
+//fun max(a: Int, b: Int): Int {
+//    return if (a > b) a else b
+//}
+
+// one liner function (expression body)
+fun max(a: Int, b: Int): Int = if (a > b) a else b
+
+fun displayMax(a: Int, b: Int) { // type is inferred to be Unit (like void in Java)
+    println(max(a, b))
+}
---
+++ src/main/kotlin/002_basics/03_Functions.kt
@@ -0,0 +1,24 @@
+package `002_basics`
+
+// You can define functions everywhere
+
+// Top-Level functions:
+// To call this from Java, you need to call it as a static function if a call, which name
+// is corresponds to the file name.
+// or, read about @JvmName annotation, example: @file:JvmName("Util"), then you can use "Util" as the
+// class name instead of the file name
+fun topLevel() = 1
+
+
+class `03_Functions` {
+
+    // Member functions
+    fun member() = 2
+
+
+    fun otherMember() {
+
+        // Local functions
+        fun local() = 3
+    }
+}
