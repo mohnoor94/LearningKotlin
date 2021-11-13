c1d3383154e402a83f1fea9254e58d3789e5532e
mohnoor94 <moh.noor94@gmail.com>
2021-11-13 22:37:57 +0200

    While and Do-While Loops

---
+++ src/main/kotlin/003_control_structures/03_While.kt
@@ -0,0 +1,28 @@
+package `003_control_structures`
+
+
+// while and do-while are the same as in Java
+
+
+fun main() {
+
+    // while
+    var i = 1
+
+    while (i < 5) {
+        println("i = $i")
+        i++
+    }
+
+
+    println("=========")
+
+    // do-while
+
+    var j = 1
+
+    do {
+        println("j = $j")
+        j++
+    } while (j < 5)
+}
