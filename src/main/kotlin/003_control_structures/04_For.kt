31a1dc0bda08ce75321fe159d0fb9d0ff2d295ba
mohnoor94 <moh.noor94@gmail.com>
2021-11-13 22:38:09 +0200

    For Loop

---
+++ src/main/kotlin/003_control_structures/04_For.kt
@@ -0,0 +1,52 @@
+package `003_control_structures`
+
+fun main() {
+
+    println("======== list ========")
+    val list = listOf("a", "b", "c")
+
+    for (s in list) { // like Python
+        println(s)
+    }
+
+    println("======== map ========")
+    val map = mapOf(
+        1 to "One",
+        2 to "Two",
+        3 to "Three"
+    )
+
+    for ((key, value) in map) {
+        println("* $key -> $value")
+    }
+
+    println("======== list with index ========")
+    for ((index, element) in list.withIndex()) {
+        println("* $index -> $element")
+    }
+
+    println("======== iterating over range ========")
+    println("..")
+    for (i in 1..9) {
+        print("$i, ")
+    } // 1..9
+    println()
+
+    println("until")
+    for (i in 1 until 9) {
+        print("$i, ")
+    } // 1..8
+    println()
+
+    println("with step / downTo")
+    for (i in 9 downTo 1 step 2) {
+        print("$i, ")
+    }
+    println()
+
+    println("======== iterating over a string ========")
+    for (ch in "abcde") {
+        print("${ch + 1}, ")
+    }
+    println()
+}
