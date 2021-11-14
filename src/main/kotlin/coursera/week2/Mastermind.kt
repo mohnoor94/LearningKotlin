c7b4d35c9bd4eda5ae0aa8a972d9c6e03ebe3526
mohnoor94 <moh.noor94@gmail.com>
2021-11-14 22:38:58 +0200

    Update syntax

--- src/main/kotlin/coursera/week2/Mastermind.kt
+++ src/main/kotlin/coursera/week2/Mastermind.kt
@@ -7,11 +7,7 @@ import kotlin.math.min
 
 fun buildFrequencyTable(str: String): HashMap<Char, Int> {
     val table = HashMap<Char, Int>()
-
-    for (ch in str) {
-        table[ch] = table.getOrDefault(ch, 0) + 1
-    }
-
+    for (ch in str) table[ch] = table.getOrDefault(ch, 0) + 1
     return table
 }
 
