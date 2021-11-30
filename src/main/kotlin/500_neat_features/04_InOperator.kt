00dcdcaf80c6bc17885a562ac957b723a6304428
mohnoor94 <moh.noor94@gmail.com>
2021-12-01 00:49:57 +0200

    Update in Operator file

--- src/main/kotlin/500_neat_features/04_InOperator.kt
+++ src/main/kotlin/500_neat_features/04_InOperator.kt
@@ -3,7 +3,7 @@ package `500_neat_features`
 // To check if an element in a collection or any object.
 // We can call `in` on any object by defining the `contains` function as operator.
 
-class CustomRange(val start: Int, val end: Int) {
+data class CustomRange(val start: Int, val end: Int) {
     operator fun contains(number: Int): Boolean {
         return (number > start) && (number <= end)
     }
