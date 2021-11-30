b19c1d28b4760754e72457f906903ce718edd5d5
mohnoor94 <moh.noor94@gmail.com>
2021-11-30 23:07:54 +0200

    Operators Overloading

---
+++ src/main/kotlin/080_object_oriented/13_OperatorsOverloading.kt
@@ -0,0 +1,42 @@
+package `080_object_oriented`
+
+import `080_object_oriented`.Digit.*
+
+// We can override some specific operators in Kotlin using:
+// - specific names.
+// - the `operator` keyword.
+
+// The Arithmetic operators we can override:
+// plus +
+// minus -
+// times *
+// div /
+// mod %
+
+// The unary operators we can override (functions without parameters):
+// unaryPlus +
+// unaryMinus -
+// not !
+// inc ++x, x++
+// dec --x, x--
+
+enum class Digit(val value: Int) {
+    ONE(1),
+    TWO(2),
+    THREE(3);
+
+    // Arithmetic
+    operator fun plus(number: Int): Int {
+        return this.value + number
+    }
+
+    // Unary
+    operator fun unaryMinus(): Int {
+        return -this.value
+    }
+}
+
+fun main() {
+    println(ONE.plus(5))
+    println(-ONE)
+}
